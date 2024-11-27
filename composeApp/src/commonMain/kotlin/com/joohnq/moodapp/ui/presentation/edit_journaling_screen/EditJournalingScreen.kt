d51bf58e86a628f83b62ed0cf94b133a1031932c
João Henrique <joaohenriquess3287@gmail.com>
2024-11-27 11:58:01 -0300

    refactor: separating edit journaling viewmodel and changing in the screens

--- composeApp/src/commonMain/kotlin/com/joohnq/moodapp/ui/presentation/edit_journaling_screen/EditJournalingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/joohnq/moodapp/ui/presentation/edit_journaling_screen/EditJournalingScreen.kt
@@ -5,13 +5,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import com.joohnq.moodapp.sharedViewModel
 import com.joohnq.moodapp.ui.CustomScreen
 import com.joohnq.moodapp.ui.presentation.edit_journaling_screen.event.EditJournalingEvent
 import com.joohnq.moodapp.ui.presentation.edit_journaling_screen.state.EditJournalingState
+import com.joohnq.moodapp.ui.state.UiState.Companion.getValue
 import com.joohnq.moodapp.ui.state.UiState.Companion.onSuccess
+import com.joohnq.moodapp.viewmodel.EditJournalingIntent
+import com.joohnq.moodapp.viewmodel.EditJournalingViewModel
 import com.joohnq.moodapp.viewmodel.HealthJournalIntent
 import com.joohnq.moodapp.viewmodel.HealthJournalViewModel
 
@@ -19,46 +23,67 @@ class EditJournalingScreen(val id: Int) : CustomScreen<EditJournalingState>() {
     @Composable
     override fun Screen(): EditJournalingState {
         val healthJournalViewModel: HealthJournalViewModel = sharedViewModel()
+        val editJournalingViewModel: EditJournalingViewModel = sharedViewModel()
         val snackBarState = remember { SnackbarHostState() }
         val healthJournalState by healthJournalViewModel.healthJournalState.collectAsState()
+        val editingHealthJournalState by editJournalingViewModel.editJournalingState.collectAsState()
+        val healthJournals = healthJournalState.healthJournalRecords.getValue()
+        val isDifferent by derivedStateOf {
+            editingHealthJournalState.editingHealthJournalRecord.title != editingHealthJournalState.currentHealthJournalRecord.title ||
+                    editingHealthJournalState.editingHealthJournalRecord.description != editingHealthJournalState.currentHealthJournalRecord.description
+        }
+        val canSave by derivedStateOf { isDifferent && editingHealthJournalState.editingHealthJournalRecord.title.isNotBlank() && editingHealthJournalState.editingHealthJournalRecord.description.isNotBlank() }
 
         fun onEvent(event: EditJournalingEvent) =
             when (event) {
                 EditJournalingEvent.OnGoBack -> onGoBack()
+                EditJournalingEvent.OnSave ->
+                    healthJournalViewModel.onAction(
+                        HealthJournalIntent.UpdateHealthJournal(
+                            editingHealthJournalState.editingHealthJournalRecord
+                        )
+                    )
             }
 
         LaunchedEffect(Unit) {
-            healthJournalViewModel.onAction(HealthJournalIntent.GetHealthJournalById(id))
+            editJournalingViewModel.onAction(
+                EditJournalingIntent.GetEditJournaling(
+                    id,
+                    healthJournals
+                )
+            )
         }
 
         LaunchedEffect(healthJournalState.deleting) {
             healthJournalState.deleting.onSuccess {
-                onGoBack()
-                healthJournalViewModel.onAction(HealthJournalIntent.ResetDeletingHeathJournal)
+                onEvent(EditJournalingEvent.OnGoBack)
+                healthJournalViewModel.onAction(HealthJournalIntent.ResetDeletingStatus)
             }
         }
 
-        LaunchedEffect(healthJournalState.editing.status) {
-            healthJournalState.editing.status.onSuccess {
+        LaunchedEffect(healthJournalState.editing) {
+            healthJournalState.editing.onSuccess {
+                onEvent(EditJournalingEvent.OnGoBack)
                 healthJournalViewModel.onAction(HealthJournalIntent.GetHealthJournals)
-                onGoBack()
             }
         }
 
         DisposableEffect(Unit) {
             onDispose {
-                healthJournalViewModel.onAction(HealthJournalIntent.ResetEditingHeathJournal)
+                editJournalingViewModel.onAction(EditJournalingIntent.ResetState)
+                healthJournalViewModel.onAction(HealthJournalIntent.ResetEditingStatus)
             }
         }
 
         return EditJournalingState(
             snackBarState = snackBarState,
-            healthJournal = healthJournalState.editing.editingHealthJournalRecord,
-            isEditing = healthJournalState.editing.isEditing,
-            isDifferent = healthJournalState.editing.editingHealthJournalRecord.title != healthJournalState.editing.currentHealthJournalRecord.title || healthJournalState.editing.editingHealthJournalRecord.description != healthJournalState.editing.currentHealthJournalRecord.description,
-            openDeleteDialog = healthJournalState.editing.openDeleteDialog,
-            onAction = healthJournalViewModel::onAction,
+            healthJournal = editingHealthJournalState.editingHealthJournalRecord,
+            isEditing = editingHealthJournalState.isEditing,
+            canSave = canSave,
+            openDeleteDialog = editingHealthJournalState.openDeleteDialog,
+            onEditingAction = editJournalingViewModel::onAction,
             onEvent = ::onEvent,
+            onAction = healthJournalViewModel::onAction
         )
     }
 
--- composeApp/src/commonMain/kotlin/com/joohnq/moodapp/ui/presentation/edit_journaling_screen/state/EditJournalingState.kt
+++ composeApp/src/commonMain/kotlin/com/joohnq/moodapp/ui/presentation/edit_journaling_screen/state/EditJournalingState.kt
@@ -3,14 +3,16 @@ package com.joohnq.moodapp.ui.presentation.edit_journaling_screen.state
 import androidx.compose.material3.SnackbarHostState
 import com.joohnq.moodapp.domain.HealthJournalRecord
 import com.joohnq.moodapp.ui.presentation.edit_journaling_screen.event.EditJournalingEvent
+import com.joohnq.moodapp.viewmodel.EditJournalingIntent
 import com.joohnq.moodapp.viewmodel.HealthJournalIntent
 
 data class EditJournalingState(
     val snackBarState: SnackbarHostState,
     val healthJournal: HealthJournalRecord,
     val isEditing: Boolean,
-    val isDifferent: Boolean,
+    val canSave: Boolean,
     val openDeleteDialog: Boolean,
-    val onEvent: (EditJournalingEvent) -> Unit,
-    val onAction: (HealthJournalIntent) -> Unit
+    val onEvent: (EditJournalingEvent) -> Unit = {},
+    val onEditingAction: (EditJournalingIntent) -> Unit = {},
+    val onAction: (HealthJournalIntent) -> Unit = {}
 )

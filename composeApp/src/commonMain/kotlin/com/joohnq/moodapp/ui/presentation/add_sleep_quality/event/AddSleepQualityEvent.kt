d52055959bbb41dbcd8a3f0271cbe2ac26032591
João Henrique <joaohenriquess3287@gmail.com>
2024-11-27 11:57:31 -0300

    refactor: separating sleep quality viewmodel and changing in the screens

--- composeApp/src/commonMain/kotlin/com/joohnq/moodapp/ui/presentation/add_sleep_quality/event/AddSleepQualityEvent.kt
+++ composeApp/src/commonMain/kotlin/com/joohnq/moodapp/ui/presentation/add_sleep_quality/event/AddSleepQualityEvent.kt
@@ -2,5 +2,6 @@ package com.joohnq.moodapp.ui.presentation.add_sleep_quality.event
 
 sealed class AddSleepQualityEvent {
     data object OnGoBack : AddSleepQualityEvent()
+    data object OnAdd : AddSleepQualityEvent()
 }
 

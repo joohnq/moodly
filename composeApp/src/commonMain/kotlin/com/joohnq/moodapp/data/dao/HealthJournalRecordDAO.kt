78b1c4be5054f99581ae5a73e2c15f8d5e7572bd
João Henrique <joaohenriquess3287@gmail.com>
2024-11-28 09:24:42 -0300

    fix: changing the repository daos

--- composeApp/src/commonMain/kotlin/com/joohnq/moodapp/data/dao/HealthJournalRecordDAO.kt
+++ composeApp/src/commonMain/kotlin/com/joohnq/moodapp/data/dao/HealthJournalRecordDAO.kt
@@ -1,26 +1,16 @@
 package com.joohnq.moodapp.data.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.joohnq.moodapp.data.DatabaseConstants
 import com.joohnq.moodapp.domain.HealthJournalRecord
-import com.joohnq.moodapp.domain.Mood
-import com.joohnq.moodapp.domain.StressLevel
-import com.joohnq.moodapp.util.helper.DatetimeManager
-import kotlinx.datetime.LocalDateTime
 
 @Dao
 interface HealthJournalRecordDAO {
-    @Query("INSERT INTO ${DatabaseConstants.HEALTH_JOURNAL_RECORD_DATABASE} (mood, title, description, ${DatabaseConstants.STRESS_LEVEL}, stressors, date) VALUES (:mood, :title, :description, :stressLevel, :stressors, :date)")
-    suspend fun addHealthJournal(
-        mood: Mood,
-        title: String,
-        description: String,
-        stressLevel: StressLevel,
-        stressors: String,
-        date: LocalDateTime = DatetimeManager.getCurrentDateTime()
-    )
+    @Insert
+    suspend fun addHealthJournal(healthJournal: HealthJournalRecord)
 
     @Query("SELECT * FROM ${DatabaseConstants.HEALTH_JOURNAL_RECORD_DATABASE} ORDER BY date DESC")
     suspend fun getHealthJournals(): List<HealthJournalRecord>

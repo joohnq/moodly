33888c3b2bec47e992f1eb9aaf1d2f691cda1ad1
João Henrique <joaohenriquess3287@gmail.com>
2025-01-02 15:29:22 -0300

    refactor: welcome ui

--- feature/welcome/ui/src/commonMain/kotlin/com/joohnq/welcome/ui/state/WelcomeState.kt
+++ feature/welcome/ui/src/commonMain/kotlin/com/joohnq/welcome/ui/state/WelcomeState.kt
@@ -2,11 +2,11 @@ package com.joohnq.welcome.ui.state
 
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.material3.SnackbarHostState
-import com.joohnq.user.ui.viewmodel.UserPreferenceViewModelIntent
+import com.joohnq.user.ui.viewmodel.user_preferences.UserPreferenceViewModelIntent
 
 data class WelcomeState(
     val snackBarState: SnackbarHostState,
     val pagerState: PagerState,
     val onNext: () -> Unit,
-    val onAction: (UserPreferenceViewModelIntent) -> Unit
+    val onAction: (UserPreferenceViewModelIntent) -> Unit,
 )

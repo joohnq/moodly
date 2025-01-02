67237fc0e0f400c9fed113d83b5b8c7006ddb6de
João Henrique <joaohenriquess3287@gmail.com>
2025-01-01 21:27:54 -0300

    refactor: shared ui adding sqldelight

--- shared/ui/src/commonMain/kotlin/com/joohnq/mood/SharedViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.joohnq.mood
-
-import androidx.compose.runtime.Composable
-import androidx.lifecycle.ViewModel
-import org.koin.compose.viewmodel.koinViewModel
-
-@Composable inline fun <reified T : ViewModel> sharedViewModel(): T {
-    return koinViewModel<T>()
-}

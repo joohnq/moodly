14d7e71f4945c3db656aa33d60297d2c64f6e3f8
João Henrique <joaohenriquess3287@gmail.com>
2025-01-04 11:07:49 -0300

    refactor: other backend

--- shared/domain/src/commonMain/kotlin/com/joohnq/shared/domain/Result.kt
+++ shared/domain/src/commonMain/kotlin/com/joohnq/shared/domain/Result.kt
@@ -2,3 +2,10 @@ package com.joohnq.shared.domain
 
 fun <T> T.toResult(): Result<T> =
     if (this is Throwable) Result.failure(this) else Result.success(this)
+
+fun <T> T?.toResultNull(message: String): Result<T> =
+    when (this) {
+        null -> Result.failure(Exception(message))
+        is Throwable -> Result.failure(this)
+        else -> Result.success(this)
+    }

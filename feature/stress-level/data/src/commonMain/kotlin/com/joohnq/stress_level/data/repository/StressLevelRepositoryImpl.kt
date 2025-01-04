965ddef5d9aa73d28390f93c084e636bf5e8ce6c
João Henrique <joaohenriquess3287@gmail.com>
2025-01-04 11:07:35 -0300

    refactor: stress level backend

--- feature/stress-level/data/src/commonMain/kotlin/com/joohnq/stress_level/data/repository/StressLevelRepositoryImpl.kt
+++ feature/stress-level/data/src/commonMain/kotlin/com/joohnq/stress_level/data/repository/StressLevelRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.joohnq.stress_level.data.repository
 
 import com.joohnq.core.database.executeTryCatchPrinting
 import com.joohnq.shared.domain.DatetimeProvider
+import com.joohnq.shared.domain.toResult
 import com.joohnq.stress_level.domain.data_source.StressLevelDataSource
 import com.joohnq.stress_level.domain.entity.StressLevelRecord
 import com.joohnq.stress_level.domain.repository.StressLevelRepository
@@ -9,10 +10,10 @@ import com.joohnq.stress_level.domain.repository.StressLevelRepository
 class StressLevelRepositoryImpl(
     private val dataSource: StressLevelDataSource,
 ) : StressLevelRepository {
-    override suspend fun getStressLevels(): List<StressLevelRecord> =
-        dataSource.getStressLevels()
+    override suspend fun getStressLevels(): Result<List<StressLevelRecord>> =
+        dataSource.getStressLevels().toResult()
 
-    override suspend fun addStressLevel(stressLevelRecord: StressLevelRecord) =
+    override suspend fun addStressLevel(stressLevelRecord: StressLevelRecord): Result<Boolean> =
         executeTryCatchPrinting {
             dataSource.addStressLevel(
                 stressLevelRecord.copy(
--- feature/stress-level/domain/src/commonMain/kotlin/com/joohnq/stress_level/domain/repository/StressLevelRepository.kt
+++ feature/stress-level/domain/src/commonMain/kotlin/com/joohnq/stress_level/domain/repository/StressLevelRepository.kt
@@ -3,6 +3,6 @@ package com.joohnq.stress_level.domain.repository
 import com.joohnq.stress_level.domain.entity.StressLevelRecord
 
 interface StressLevelRepository {
-    suspend fun getStressLevels(): List<StressLevelRecord>
-    suspend fun addStressLevel(stressLevelRecord: StressLevelRecord): Boolean
+    suspend fun getStressLevels(): Result<List<StressLevelRecord>>
+    suspend fun addStressLevel(stressLevelRecord: StressLevelRecord): Result<Boolean>
 }

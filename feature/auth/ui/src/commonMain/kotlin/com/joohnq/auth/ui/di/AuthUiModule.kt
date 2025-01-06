51459e842e1d8ca0254451c7055a0e954f5ba90b
João Henrique <joaohenriquess3287@gmail.com>
2025-01-06 14:32:17 -0300

    feat: creating auth module

---
+++ feature/auth/ui/src/commonMain/kotlin/com/joohnq/auth/ui/di/AuthUiModule.kt
@@ -0,0 +1,5 @@
+package com.joohnq.auth.ui.di
+
+import org.koin.core.module.Module
+
+expect val authUiModule: Module
---
+++ feature/auth/ui/src/commonMain/kotlin/com/joohnq/auth/ui/presentation/user_name/viewmodel/UserNameViewModel.kt
@@ -0,0 +1,36 @@
+package com.joohnq.auth.ui.presentation.user_name.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+
+class UserNameViewModel : ViewModel() {
+    private val _state: MutableStateFlow<UserNameViewModelState> =
+        MutableStateFlow(UserNameViewModelState())
+    val state: StateFlow<UserNameViewModelState> = _state
+
+    fun onAction(intent: UserNameIntent) {
+        when (intent) {
+            is UserNameIntent.UpdateUserName -> updateUserName(intent.name)
+            UserNameIntent.ResetState -> resetState()
+            is UserNameIntent.UpdateUserNameError -> updateUserNameError(intent.error)
+        }
+    }
+
+    private fun updateUserName(name: String) {
+        _state.update {
+            it.copy(name = name, nameError = null)
+        }
+    }
+
+    private fun updateUserNameError(error: String?) {
+        _state.update {
+            it.copy(nameError = error)
+        }
+    }
+
+    private fun resetState() {
+        _state.update { UserNameViewModelState() }
+    }
+}

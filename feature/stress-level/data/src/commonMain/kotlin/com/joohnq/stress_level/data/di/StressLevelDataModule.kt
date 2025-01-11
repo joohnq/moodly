0ef529d49a12e01f72ed974567d791de6180e962
João Henrique <joaohenriquess3287@gmail.com>
2025-01-11 19:39:03 -0300

    fix: removing the stress level data source and put the login only in repository

--- feature/stress-level/data/src/commonMain/kotlin/com/joohnq/stress_level/data/di/StressLevelDataModule.kt
+++ feature/stress-level/data/src/commonMain/kotlin/com/joohnq/stress_level/data/di/StressLevelDataModule.kt
@@ -1,9 +1,7 @@
 package com.joohnq.stress_level.data.di
 
-import com.joohnq.stress_level.data.data_source.StressLevelDataSourceImpl
 import com.joohnq.stress_level.data.database.StressLevelDatabase
 import com.joohnq.stress_level.data.repository.StressLevelRepositoryImpl
-import com.joohnq.stress_level.domain.data_source.StressLevelDataSource
 import com.joohnq.stress_level.domain.repository.StressLevelRepository
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
@@ -12,6 +10,5 @@ import org.koin.dsl.module
 val stressLevelDataModule = module {
     singleOf(::StressLevelDatabase)
     singleOf(StressLevelDatabase::invoke)
-    singleOf(::StressLevelDataSourceImpl) bind StressLevelDataSource::class
     singleOf(::StressLevelRepositoryImpl) bind StressLevelRepository::class
 }
--- feature/stress-level/domain/src/commonMain/kotlin/com/joohnq/stress_level/domain/data_source/StressLevelDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.joohnq.stress_level.domain.data_source
-
-import com.joohnq.stress_level.domain.entity.StressLevelRecord
-
-interface StressLevelDataSource {
-    suspend fun getStressLevels(): List<StressLevelRecord>
-    suspend fun addStressLevel(stressLevelRecord: StressLevelRecord)
-}

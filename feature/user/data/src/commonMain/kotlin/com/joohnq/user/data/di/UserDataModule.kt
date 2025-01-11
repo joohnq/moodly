adbe1475bbf791a2db94d072ded022adff6e8b2f
João Henrique <joaohenriquess3287@gmail.com>
2025-01-11 19:39:11 -0300

    fix: removing the user data source and put the login only in repository

--- feature/user/data/src/commonMain/kotlin/com/joohnq/user/data/di/UserDataModule.kt
+++ feature/user/data/src/commonMain/kotlin/com/joohnq/user/data/di/UserDataModule.kt
@@ -1,11 +1,7 @@
 package com.joohnq.user.data.di
 
-import com.joohnq.domain.data_source.UserDataSource
-import com.joohnq.domain.data_source.UserPreferencesDataSource
 import com.joohnq.domain.repository.UserPreferencesRepository
 import com.joohnq.domain.repository.UserRepository
-import com.joohnq.user.data.data_source.UserDataSourceImpl
-import com.joohnq.user.data.data_source.UserPreferencesDataSourceImpl
 import com.joohnq.user.data.database.UserDatabase
 import com.joohnq.user.data.repository.UserPreferencesRepositoryImpl
 import com.joohnq.user.data.repository.UserRepositoryImpl
@@ -16,8 +12,6 @@ import org.koin.dsl.module
 val userDataModule = module {
     singleOf(::UserDatabase)
     singleOf(UserDatabase::invoke)
-    singleOf(::UserDataSourceImpl) bind UserDataSource::class
-    singleOf(::UserPreferencesDataSourceImpl) bind UserPreferencesDataSource::class
     singleOf(::UserRepositoryImpl) bind UserRepository::class
     singleOf(::UserPreferencesRepositoryImpl) bind UserPreferencesRepository::class
 }

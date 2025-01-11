8e0da05cb658b76c3595ce333fb14f2f94d7264c
João Henrique <joaohenriquess3287@gmail.com>
2025-01-11 19:38:18 -0300

    fix: removing the mood data source and put the login only in repository

--- feature/mood/data/src/commonMain/kotlin/com/joohnq/mood/data/repository/StatsRepositoryImpl.kt
+++ feature/mood/data/src/commonMain/kotlin/com/joohnq/mood/data/repository/StatsRepositoryImpl.kt
@@ -1,24 +1,42 @@
 package com.joohnq.mood.data.repository
 
-import com.joohnq.core.database.executeTryCatchPrinting
+import com.joohnq.core.database.converters.LocalDateTimeConverter
+import com.joohnq.core.database.executeTryCatchResult
 import com.joohnq.core.ui.DatetimeProvider
-import com.joohnq.core.ui.toResult
-import com.joohnq.mood.domain.data_source.StatsDataSource
+import com.joohnq.mood.database.StatsDatabaseSql
+import com.joohnq.mood.domain.converter.StatsRecordConverter
 import com.joohnq.mood.domain.entity.StatsRecord
 import com.joohnq.mood.domain.repository.StatsRepository
 
 class StatsRepositoryImpl(
-    private val dataSource: StatsDataSource,
+    private val database: StatsDatabaseSql,
 ) : StatsRepository {
-    override suspend fun getStats(): Result<List<StatsRecord>> = dataSource.getStats().toResult()
+    private val query = database.statRecordQueries
+    override suspend fun getStats(): Result<List<StatsRecord>> =
+        executeTryCatchResult {
+            query.getStats { id, mood, description, date ->
+                StatsRecord(
+                    id = id.toInt(),
+                    mood = StatsRecordConverter.toMood(mood),
+                    description = description,
+                    date = LocalDateTimeConverter.toLocalDateTime(date)
+                )
+            }.executeAsList()
+        }
 
     override suspend fun addStats(statsRecord: StatsRecord): Result<Boolean> =
-        executeTryCatchPrinting {
-            dataSource.addStats(statsRecord.copy(date = DatetimeProvider.getCurrentDateTime()))
+        executeTryCatchResult {
+            val item = statsRecord.copy(date = DatetimeProvider.getCurrentDateTime())
+            query.addStats(
+                mood = StatsRecordConverter.fromMood(item.mood),
+                description = statsRecord.description
+            )
+            true
         }
 
     override suspend fun deleteStat(id: Int): Result<Boolean> =
-        executeTryCatchPrinting {
-            dataSource.deleteStat(id)
+        executeTryCatchResult {
+            query.deleteStat(id.toLong())
+            true
         }
 }
--- feature/mood/domain/src/commonMain/kotlin/com/joohnq/mood/domain/data_source/StatsDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.joohnq.mood.domain.data_source
-
-import com.joohnq.mood.domain.entity.StatsRecord
-
-interface StatsDataSource {
-    suspend fun getStats(): List<StatsRecord>
-    suspend fun addStats(statsRecord: StatsRecord)
-    suspend fun deleteStat(id: Int)
-}

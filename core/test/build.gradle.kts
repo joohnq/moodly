c8fa8806b8b48c8b6946cbcd0dd4dd2f8f45844a
João Henrique <joaohenriquess3287@gmail.com>
2025-01-12 13:49:49 -0300

    feat: creating a core test for common resources

---
+++ core/test/build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "test"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.androidx.startup.runtime)
+            implementation(libs.android.driver)
+
+            implementation(libs.androidx.core.ktx)
+            implementation(libs.robolectric)
+            implementation(libs.junit)
+        }
+        commonMain.dependencies {
+            implementation(projects.core.database)
+            implementation(libs.bundles.test)
+            implementation(libs.coroutines.extensions)
+        }
+        iosMain.dependencies {
+            implementation(libs.sqldelight.native.driver)
+        }
+        nativeMain.dependencies {
+            implementation(libs.sqldelight.native.driver)
+        }
+    }
+}
+
+android {
+    namespace = "com.joohnq.core.test"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}

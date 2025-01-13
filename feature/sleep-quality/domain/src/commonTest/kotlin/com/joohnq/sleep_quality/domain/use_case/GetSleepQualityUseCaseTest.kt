19c44ba9b7f2d767f39a3c138213a4c350ae3314
João Henrique <joaohenriquess3287@gmail.com>
2025-01-13 10:19:42 -0300

    feat: create sleep quality use cases test

---
+++ feature/sleep-quality/domain/src/commonTest/kotlin/com/joohnq/sleep_quality/domain/use_case/GetSleepQualityUseCaseTest.kt
@@ -0,0 +1,46 @@
+package com.joohnq.sleep_quality.domain.use_case
+
+import com.joohnq.sleep_quality.domain.entity.SleepQuality
+import com.joohnq.sleep_quality.domain.entity.SleepQualityRecord
+import com.joohnq.sleep_quality.domain.fake.SleepQualityRepositoryFake
+import com.varabyte.truthish.assertThat
+import kotlinx.coroutines.runBlocking
+import kotlinx.datetime.LocalDateTime
+import kotlin.test.Test
+
+class GetSleepQualityUseCaseTest {
+    private val repository: SleepQualityRepositoryFake = SleepQualityRepositoryFake()
+    private val useCase: GetSleepQualitiesUseCase = GetSleepQualitiesUseCase(repository)
+
+    @Test
+    fun `GIVEN a valid request WHEN calling getSleepQualitiesUseCase THEN should return true`() =
+        runBlocking {
+            //WHEN
+            val res = useCase.invoke().getOrNull()
+
+            //THEN
+            assertThat(res).isEqualTo(
+                listOf(
+                    SleepQualityRecord(
+                        id = 1,
+                        sleepQuality = SleepQuality.Fair,
+                        startSleeping = "00:00",
+                        endSleeping = "06:00",
+                        sleepInfluences = listOf(),
+                        date = LocalDateTime(2025, 1, 1, 0, 0, 0)
+                    )
+                )
+            )
+        }
+
+    @Test
+    fun `GIVEN a invalid request WHEN calling getSleepQualitiesUseCase THEN should return exception in failure`() =
+        runBlocking {
+            repository.updateShouldThrowError(true)
+            //WHEN
+            val res = useCase.invoke().exceptionOrNull()
+
+            //THEN
+            assertThat(res?.message).isEqualTo("Failed to get sleep qualities")
+        }
+}

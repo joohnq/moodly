51903b474fbc148df2ab482e8bdc6d91faf7e3cb
João Henrique <joaohenriquess3287@gmail.com>
2025-01-19 20:34:54 -0300

    fix: security screen

--- feature/security/ui/src/commonMain/kotlin/com/joohnq/security/ui/presentation/security/event/SecurityEvent.kt
+++ feature/security/ui/src/commonMain/kotlin/com/joohnq/security/ui/presentation/security/event/SecurityEvent.kt
@@ -2,5 +2,6 @@ package com.joohnq.security.ui.presentation.security.event
 
 sealed class SecurityEvent {
     data object OnContinue : SecurityEvent()
+    data object OnSetPin : SecurityEvent()
     data object OnSkip : SecurityEvent()
 }

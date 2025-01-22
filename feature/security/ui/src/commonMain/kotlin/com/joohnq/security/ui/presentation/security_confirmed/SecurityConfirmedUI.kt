64f2e0679592e6aa4b35fbb7f41173dae8458ce8
João Henrique <joaohenriquess3287@gmail.com>
2025-01-22 11:17:37 -0300

    feat: creating security confirmed

--- feature/security/ui/src/commonMain/kotlin/com/joohnq/security/ui/presentation/security_confirmed/SecurityConfirmedUI.kt
+++ feature/security/ui/src/commonMain/kotlin/com/joohnq/security/ui/presentation/security_confirmed/SecurityConfirmedUI.kt
@@ -1,30 +1,21 @@
 package com.joohnq.security.ui.presentation.security_confirmed
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.joohnq.security.ui.presentation.security_confirmed.event.SecurityConfirmedEvent
-import com.joohnq.security.ui.presentation.security_confirmed.state.SecurityConfirmedState
-import com.joohnq.shared_resources.ConvexContentLayout
 import com.joohnq.shared_resources.Res
 import com.joohnq.shared_resources.components.ContinueButton
+import com.joohnq.shared_resources.components.DecoratedConvexPanel
 import com.joohnq.shared_resources.components.VerticalSpacer
 import com.joohnq.shared_resources.now_every_time_you_open_the_app
 import com.joohnq.shared_resources.security_setup_completed
@@ -36,54 +27,12 @@ import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
-fun Shared(
-    panelBackgroundColor: Color,
-    backgroundColor: Color = Colors.Brown10,
-    panelContent: @Composable () -> Unit,
-    content: @Composable (ColumnScope) -> Unit,
+fun SecurityConfirmedUI(
+    onEvent: (SecurityConfirmedEvent) -> Unit,
 ) {
-    Scaffold(
-        containerColor = backgroundColor,
-        modifier = Modifier.fillMaxSize()
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .padding(padding)
-                .padding(bottom = 20.dp)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.SpaceBetween,
-        ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.6f)
-                        .background(color = panelBackgroundColor),
-                    contentAlignment = Alignment.BottomCenter
-                ) {
-                    panelContent()
-                    ConvexContentLayout(
-                        backgroundColor = backgroundColor,
-                    )
-                }
-                Column(
-                    verticalArrangement = Arrangement.SpaceBetween,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    content(this)
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun SecurityConfirmedUI(state: SecurityConfirmedState) {
-    Shared(
+    DecoratedConvexPanel(
         panelBackgroundColor = Colors.Green20,
+        backgroundColor = Colors.White,
         panelContent = {
             Image(
                 painter = painterResource(Drawables.Images.SecurityConfirmedBackground),
@@ -120,7 +69,7 @@ fun SecurityConfirmedUI(state: SecurityConfirmedState) {
             }
             ContinueButton(
                 modifier = Modifier.fillMaxWidth().paddingHorizontalMedium(),
-                onClick = { state.onEvent(SecurityConfirmedEvent.OnContinue) }
+                onClick = { onEvent(SecurityConfirmedEvent.OnContinue) }
             )
         }
     )
--- feature/security/ui/src/commonMain/kotlin/com/joohnq/security/ui/presentation/security_confirmed/state/SecurityConfirmedState.kt
+++
@@ -1,7 +0,0 @@
-package com.joohnq.security.ui.presentation.security_confirmed.state
-
-import com.joohnq.security.ui.presentation.security_confirmed.event.SecurityConfirmedEvent
-
-data class SecurityConfirmedState(
-    val onEvent: (SecurityConfirmedEvent) -> Unit,
-)

155d7c5fddcfd7e37a18b0ad3c890995ef459790
João Henrique <joaohenriquess3287@gmail.com>
2025-01-28 14:24:50 -0300

    feat: changes in freud score ui

--- feature/freud-score/ui/src/commonMain/kotlin/com/joohnq/freud_score/ui/presentation/freud_score/FreudScoreUI.kt
+++ feature/freud-score/ui/src/commonMain/kotlin/com/joohnq/freud_score/ui/presentation/freud_score/FreudScoreUI.kt
@@ -2,14 +2,19 @@ package com.joohnq.freud_score.ui.presentation.freud_score
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.joohnq.core.ui.entity.UiState
 import com.joohnq.core.ui.mapper.foldComposable
 import com.joohnq.core.ui.mapper.forEachMap
@@ -26,10 +31,10 @@ import com.joohnq.shared_resources.components.SharedPanelComponent
 import com.joohnq.shared_resources.components.SmallTitle
 import com.joohnq.shared_resources.freud_score
 import com.joohnq.shared_resources.mental_score_history
-import com.joohnq.shared_resources.theme.Colors
 import com.joohnq.shared_resources.theme.Drawables
 import com.joohnq.shared_resources.theme.PaddingModifier.Companion.paddingHorizontalMedium
 import com.joohnq.shared_resources.theme.TextStyles
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
 
@@ -63,15 +68,30 @@ fun FreudScoreUI(
                         horizontalAlignment = Alignment.CenterHorizontally,
                         verticalArrangement = Arrangement.Center
                     ) {
-                        Text(
-                            text = freudScore.score.toString(),
-                            style = TextStyles.DisplayMdExtraBold(),
-                            color = Colors.White
-                        )
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.spacedBy(
+                                12.dp,
+                                alignment = Alignment.CenterHorizontally
+                            )
+                        ) {
+                            Icon(
+                                painter = painterResource(Drawables.Icons.Logo),
+                                contentDescription = null,
+                                modifier = Modifier.size(64.dp),
+                                tint = freudScore.palette.subColor
+                            )
+                            Text(
+                                text = freudScore.score.toString(),
+                                style = TextStyles.DisplayMdExtraBold(),
+                                color = freudScore.palette.subColor
+                            )
+                        }
                         Text(
                             text = stringResource(freudScore.title),
                             style = TextStyles.TextXlSemiBold(),
-                            color = Colors.White
+                            color = freudScore.palette.subColor
                         )
                     }
                 },

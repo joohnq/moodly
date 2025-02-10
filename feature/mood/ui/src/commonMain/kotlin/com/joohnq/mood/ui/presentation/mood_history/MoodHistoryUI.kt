bbf13aba7e0b1c8c5ecb2dbaff982c92dc3791ae
João Henrique <joaohenriquess3287@gmail.com>
2025-02-10 08:21:36 -0300

    feat: creating mood history screen

--- feature/mood/ui/src/commonMain/kotlin/com/joohnq/mood/ui/presentation/mood_history/MoodHistoryUI.kt
+++ feature/mood/ui/src/commonMain/kotlin/com/joohnq/mood/ui/presentation/mood_history/MoodHistoryUI.kt
@@ -1,10 +1,11 @@
-package com.joohnq.stress_level.ui.presentation.stress_history
+package com.joohnq.mood.ui.presentation.mood_history
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,43 +13,40 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.joohnq.core.ui.entity.UiState
 import com.joohnq.core.ui.mapper.foldComposable
-import com.joohnq.core.ui.mapper.items
-import com.joohnq.core.ui.mapper.toFormattedDateString
+import com.joohnq.mood.ui.components.MoodHistoryCard
+import com.joohnq.mood.ui.presentation.mood_history.event.MoodHistoryEvent
+import com.joohnq.mood.ui.resource.MoodRecordResource
+import com.joohnq.mood.ui.resource.MoodResource
 import com.joohnq.shared_resources.Res
 import com.joohnq.shared_resources.all_history
-import com.joohnq.shared_resources.components.IsEmpty
 import com.joohnq.shared_resources.components.SwipeTorRevealCard
 import com.joohnq.shared_resources.components.TopBar
 import com.joohnq.shared_resources.components.VerticalSpacer
 import com.joohnq.shared_resources.theme.Colors
 import com.joohnq.shared_resources.theme.PaddingModifier.Companion.paddingHorizontalMedium
 import com.joohnq.shared_resources.theme.TextStyles
-import com.joohnq.stress_level.ui.component.StressLevelHistoryCard
-import com.joohnq.stress_level.ui.mapper.toGroupedByDate
-import com.joohnq.stress_level.ui.presentation.stress_history.event.StressHistoryEvent
-import com.joohnq.stress_level.ui.resource.StressLevelRecordResource
-import com.joohnq.stress_level.ui.viewmodel.StressLevelState
+import kotlinx.datetime.LocalDateTime
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-fun StressHistoryUI(
-    state: StressLevelState,
-    onEvent: (StressHistoryEvent) -> Unit,
+fun MoodHistoryUI(
+    records: UiState<List<MoodRecordResource>>,
+    onEvent: (MoodHistoryEvent) -> Unit = {},
 ) {
-    state.records.foldComposable(
+    records.foldComposable(
         onSuccess = { records ->
-            val recordsMap = records.toGroupedByDate()
             Scaffold(
                 containerColor = Colors.Brown10
             ) { padding ->
-                Column(modifier = Modifier
-                    .padding(padding).paddingHorizontalMedium()
+                Column(
+                    modifier = Modifier
+                        .padding(padding).paddingHorizontalMedium()
                 ) {
                     TopBar(
                         modifier = Modifier.fillMaxWidth(),
                         isDark = true,
-                        onGoBack = { onEvent(StressHistoryEvent.OnGoBack) }
+                        onGoBack = { onEvent(MoodHistoryEvent.OnGoBack) }
                     )
                     VerticalSpacer(20.dp)
                     Text(
@@ -61,24 +59,12 @@ fun StressHistoryUI(
                         modifier = Modifier.fillMaxWidth(),
                         verticalArrangement = Arrangement.spacedBy(10.dp)
                     ) {
-                        items(
-                            items = recordsMap,
-                            empty = {
-                                IsEmpty()
-                            },
-                            title = { date ->
-                                Text(
-                                    text = date.toFormattedDateString(),
-                                    style = TextStyles.TextMdBold(),
-                                    color = Colors.Gray80
-                                )
-                            },
-                        ) { record ->
+                        items(records) { record ->
                             SwipeTorRevealCard(
                                 modifier = Modifier.fillMaxWidth(),
-                                onAction = {}
+                                onAction = {},
                             ) { modifier ->
-                                StressLevelHistoryCard(
+                                MoodHistoryCard(
                                     modifier = modifier,
                                     containerColor = Colors.White,
                                     record = record,
@@ -94,32 +80,79 @@ fun StressHistoryUI(
 
 @Preview
 @Composable
-fun StressHistoryUIPreview() {
-    StressHistoryUI(
-        state = StressLevelState(
-            records = UiState.Success(
-                listOf(
-                    StressLevelRecordResource(),
-                    StressLevelRecordResource(),
-                    StressLevelRecordResource(),
-                    StressLevelRecordResource(),
-                    StressLevelRecordResource(),
+fun MoodHistoryUIPreviewEmpty() {
+    MoodHistoryUI(
+        records = UiState.Success(
+            listOf(
+                MoodRecordResource(
+                    createdAt = LocalDateTime(2025, 1, 1, 0, 0, 0)
                 )
             )
         ),
-        onEvent = {}
+        onEvent = {},
     )
 }
 
 @Preview
 @Composable
-fun StressHistoryUIPreviewEmpty() {
-    StressHistoryUI(
-        state = StressLevelState(
-            records = UiState.Success(
-                listOf()
+fun MoodHistoryUIPreviewDepressed() {
+    MoodHistoryUI(
+        records = UiState.Success(
+            listOf(
+                MoodRecordResource(
+                    mood = MoodResource.Depressed,
+                    description = "Description"
+                ),
             )
         ),
-        onEvent = {}
+        onEvent = {},
+    )
+}
+
+@Preview
+@Composable
+fun MoodHistoryUIPreviewNeutral() {
+    MoodHistoryUI(
+        records = UiState.Success(
+            listOf(
+                MoodRecordResource(
+                    mood = MoodResource.Neutral,
+                    description = "Description"
+                ),
+            )
+        ),
+        onEvent = {},
+    )
+}
+
+@Preview
+@Composable
+fun MoodHistoryUIPreviewHappy() {
+    MoodHistoryUI(
+        records = UiState.Success(
+            listOf(
+                MoodRecordResource(
+                    mood = MoodResource.Happy,
+                    description = "Description"
+                ),
+            )
+        ),
+        onEvent = {},
+    )
+}
+
+@Preview
+@Composable
+fun MoodHistoryUIPreviewOverjoyed() {
+    MoodHistoryUI(
+        records = UiState.Success(
+            listOf(
+                MoodRecordResource(
+                    mood = MoodResource.Overjoyed,
+                    description = "Description"
+                ),
+            )
+        ),
+        onEvent = {},
     )
 }

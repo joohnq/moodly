8bcfbd607593c552a6855abaeeddaee6b38490f3
João Henrique <joaohenriquess3287@gmail.com>
2025-03-25 09:20:09 -0300

    fix: date translation, fix: blocking click when central is expanded, fix: some translations

--- feature/stress-level/ui/src/commonMain/kotlin/com/joohnq/stress_level/ui/viewmodel/StressLevelSideEffect.kt
+++ feature/stress-level/ui/src/commonMain/kotlin/com/joohnq/stress_level/ui/viewmodel/StressLevelSideEffect.kt
@@ -3,5 +3,5 @@ package com.joohnq.stress_level.ui.viewmodel
 sealed interface StressLevelSideEffect {
     data object StressLevelAdded : StressLevelSideEffect
     data object StressLevelDeleted : StressLevelSideEffect
-    data class ShowError(val error: String) : StressLevelSideEffect
+    data class ShowError(val error: Throwable) : StressLevelSideEffect
 } 

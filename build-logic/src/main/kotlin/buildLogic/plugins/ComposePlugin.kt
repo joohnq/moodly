a313fdb1922f8716aaa8f6152bf2416c30d28f3b
João Henrique <joaohenriquess3287@gmail.com>
2025-07-16 13:12:44 -0300

    feat: creating compose build logic

---
+++ build-logic/src/main/kotlin/buildLogic/plugins/ComposePlugin.kt
@@ -0,0 +1,35 @@
+package buildLogic.plugins
+
+import buildLogic.configs.AppConfig
+import buildLogic.extensions.getPlugin
+import com.android.build.gradle.LibraryExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class ComposePlugin : Plugin<Project> {
+
+    override fun apply(target: Project) = with(target) {
+        installPlugins()
+        androidSettings()
+    }
+
+    private fun Project.installPlugins() {
+        pluginManager.apply(
+            getPlugin(alias = "composeMultiplatform").pluginId
+        )
+        pluginManager.apply(
+            getPlugin(alias = "composeCompiler").pluginId
+        )
+    }
+
+    private fun Project.androidSettings() {
+        extensions.configure<LibraryExtension> {
+            compileSdk = AppConfig.compileSdk
+
+            buildFeatures {
+                compose = true
+            }
+        }
+    }
+}

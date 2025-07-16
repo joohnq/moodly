964e86196806f417ab739951de25a3d159050414
João Henrique <joaohenriquess3287@gmail.com>
2025-07-16 13:12:12 -0300

    feat: creating init build logic

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ rootProject.name = "Moodly"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
+    includeBuild("build-logic")
     repositories {
         google {
             mavenContent {

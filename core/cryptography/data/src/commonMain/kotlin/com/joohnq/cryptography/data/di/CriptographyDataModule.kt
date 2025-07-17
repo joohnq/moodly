25ba3812ea1dd14eaed36eb887e798d433524d6e
João Henrique <joaohenriquess3287@gmail.com>
2025-07-16 21:03:35 -0300

    remove: removing cryptography module

--- core/cryptography/data/src/commonMain/kotlin/com/joohnq/cryptography/data/di/CriptographyDataModule.kt
+++
@@ -1,5 +0,0 @@
-package com.joohnq.cryptography.data.di
-
-import org.koin.core.module.Module
-
-expect val cryptographyDataModule: Module
--- core/cryptography/domain/src/commonMain/kotlin/com/joohnq/cryptography/domain/KeyGeneration.kt
+++
@@ -1,7 +0,0 @@
-package com.joohnq.cryptography.domain
-
-import dev.whyoleg.cryptography.algorithms.AES
-
-interface KeyGeneration {
-    suspend operator fun invoke(): AES.GCM.Key
-}

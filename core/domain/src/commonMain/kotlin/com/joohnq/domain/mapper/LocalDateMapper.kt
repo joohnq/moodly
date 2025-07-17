25ec2a6144812f2c17b2569fa06d492141593855
João Henrique <joaohenriquess3287@gmail.com>
2025-07-16 22:06:00 -0300

    fix: update date formatting to use padded day and correct imports for consistency and correctness

--- core/domain/src/commonMain/kotlin/com/joohnq/domain/mapper/LocalDateMapper.kt
+++ core/domain/src/commonMain/kotlin/com/joohnq/domain/mapper/LocalDateMapper.kt
@@ -4,13 +4,14 @@ import kotlinx.datetime.LocalDate
 import kotlinx.datetime.format
 import kotlinx.datetime.format.DayOfWeekNames
 import kotlinx.datetime.format.MonthNames
+import kotlinx.datetime.format.Padding
 import kotlinx.datetime.format.char
 
 fun LocalDate.toFormattedDateString(): String =
     format(LocalDate.Format {
         dayOfWeek(DayOfWeekNames.ENGLISH_ABBREVIATED)
         chars(", ")
-        dayOfMonth()
+        day(padding = Padding.ZERO)
         char(' ')
         monthName(MonthNames.ENGLISH_ABBREVIATED)
         char(' ')
@@ -21,14 +22,14 @@ fun LocalDate.toMonthCompleteDayAndYear(): String =
     format(LocalDate.Format {
         monthName(MonthNames.ENGLISH_FULL)
         chars(" ")
-        dayOfMonth()
+        day(padding = Padding.ZERO)
         chars(", ")
         year()
     })
 
 fun LocalDate.toShortDateString(): String =
     format(LocalDate.Format {
-        dayOfMonth()
+        day(padding = Padding.ZERO)
         chars("/")
         monthNumber()
     })
@@ -37,11 +38,11 @@ fun LocalDate.toMonthAbbreviatedAndDayString(): String =
     format(LocalDate.Format {
         monthName(MonthNames.ENGLISH_ABBREVIATED)
         chars(" ")
-        dayOfMonth()
+        day(padding = Padding.ZERO)
     })
 
 fun LocalDate.toCompleteDateString(): String = format(LocalDate.Format {
-    dayOfMonth()
+    day(padding = Padding.ZERO)
     chars("/")
     monthNumber()
     chars("/")
--- core/domain/src/commonMain/kotlin/com/joohnq/domain/mapper/LocalDateTimeMapper.kt
+++ core/domain/src/commonMain/kotlin/com/joohnq/domain/mapper/LocalDateTimeMapper.kt
@@ -1,30 +1,14 @@
 package com.joohnq.domain.mapper
 
-import com.joohnq.domain.entity.DaySection
 import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.format
-import kotlinx.datetime.format.DayOfWeekNames
 import kotlinx.datetime.format.MonthNames
+import kotlinx.datetime.format.Padding
 import kotlinx.datetime.format.char
 import kotlinx.datetime.plus
 import kotlinx.datetime.until
-
-fun LocalDateTime.toFormattedDateTimeString(): String =
-    format(LocalDateTime.Format {
-        dayOfWeek(DayOfWeekNames.ENGLISH_ABBREVIATED)
-        chars(", ")
-        dayOfMonth()
-        char(' ')
-        monthName(MonthNames.ENGLISH_ABBREVIATED)
-        char(' ')
-        year()
-        chars(" at ")
-        hour()
-        char(':')
-        minute()
-    })
 
 fun LocalDateTime.toMonthNameString(): String =
     format(LocalDateTime.Format {
@@ -35,14 +19,14 @@ fun LocalDateTime.toMonthAbbreviatedAndDayString(): String =
     format(LocalDateTime.Format {
         monthName(MonthNames.ENGLISH_ABBREVIATED)
         chars(" ")
-        dayOfMonth()
+        day(padding = Padding.ZERO)
     })
 
 fun LocalDateTime.toMonthAbbreviatedDayAndHourFormatted(): String =
     format(LocalDateTime.Format {
         monthName(MonthNames.ENGLISH_ABBREVIATED)
         chars(" ")
-        dayOfMonth()
+        day(padding = Padding.ZERO)
         chars("\nat ")
         hour()
         char(':')
@@ -55,19 +39,9 @@ fun LocalDateTime.toMonthDays(): Int {
     return start.until(end, DateTimeUnit.DAY).toInt()
 }
 
-fun LocalDateTime.getCurrentWeekDayIndex(): Int =
-    LocalDate(date.year, date.month, 1).dayOfWeek.ordinal
-
 fun LocalDateTime.toFormattedTimeString(): String =
     format(LocalDateTime.Format {
         hour()
         char(':')
         minute()
     })
-
-fun LocalDateTime.getDayPeriod(): String =
-    when (hour) {
-        in 0..11 -> DaySection.Morning.text
-        in 12..17 -> DaySection.Afternoon.text
-        else -> DaySection.Evening.text
-    }

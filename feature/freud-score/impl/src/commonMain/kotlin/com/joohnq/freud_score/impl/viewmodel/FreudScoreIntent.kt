b803a71c2404192af6610f3adc29b3dc300aea08
João Henrique <joaohenriquess3287@gmail.com>
2025-07-22 09:39:34 -0300

    fix: module :feature:freud-score imports

--- feature/freud-score/impl/src/commonMain/kotlin/com/joohnq/freud_score/impl/viewmodel/FreudScoreIntent.kt
+++ feature/freud-score/impl/src/commonMain/kotlin/com/joohnq/freud_score/impl/viewmodel/FreudScoreIntent.kt
@@ -1,6 +1,6 @@
 package com.joohnq.freud_score.impl.viewmodel
 
-import com.joohnq.mood.ui.resource.MoodRecordResource
+import com.joohnq.mood.impl.ui.resource.MoodRecordResource
 
 sealed interface FreudScoreIntent {
     data class GetFreudScore(val records: List<MoodRecordResource>) : FreudScoreIntent

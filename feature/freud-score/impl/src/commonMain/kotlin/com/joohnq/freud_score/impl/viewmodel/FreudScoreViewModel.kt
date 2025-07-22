c57a79d353ed87489e707f49756f945c5e595922
João Henrique <joaohenriquess3287@gmail.com>
2025-07-22 09:39:34 -0300

    fix: module :feature:freud-score imports

--- feature/freud-score/impl/src/commonMain/kotlin/com/joohnq/freud_score/impl/viewmodel/FreudScoreViewModel.kt
+++ feature/freud-score/impl/src/commonMain/kotlin/com/joohnq/freud_score/impl/viewmodel/FreudScoreViewModel.kt
@@ -2,8 +2,8 @@ package com.joohnq.freud_score.impl.viewmodel
 
 import androidx.lifecycle.ViewModel
 import com.joohnq.freud_score.impl.mapper.toResource
-import com.joohnq.mood.ui.mapper.calculateStatsFreudScore
-import com.joohnq.mood.ui.resource.MoodRecordResource
+import com.joohnq.mood.impl.ui.mapper.calculateStatsFreudScore
+import com.joohnq.mood.impl.ui.resource.MoodRecordResource
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow

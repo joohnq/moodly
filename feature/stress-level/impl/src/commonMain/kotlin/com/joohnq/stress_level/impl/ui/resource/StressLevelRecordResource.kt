e9380d555cdd5badc5da573891a386edb49ca785
João Henrique <joaohenriquess3287@gmail.com>
2025-07-23 13:58:15 -0300

    feat(:feature:stressLevel:impl): creating preview fake

--- feature/stress-level/impl/src/commonMain/kotlin/com/joohnq/stress_level/impl/ui/resource/StressLevelRecordResource.kt
+++ feature/stress-level/impl/src/commonMain/kotlin/com/joohnq/stress_level/impl/ui/resource/StressLevelRecordResource.kt
@@ -1,6 +1,7 @@
 package com.joohnq.stress_level.impl.ui.resource
 
 import com.joohnq.api.getNow
+import com.joohnq.stress_level.impl.ui.resource.StressorResource.Companion.allStressors
 import kotlinx.datetime.LocalDateTime
 
 data class StressLevelRecordResource(
@@ -8,4 +9,37 @@ data class StressLevelRecordResource(
     val stressLevel: StressLevelResource = StressLevelResource.One,
     val stressors: List<StressorResource> = emptyList(),
     val createdAt: LocalDateTime = getNow(),
-)
+){
+    companion object{
+        val stressLevelRecordResourceOnePreview = StressLevelRecordResource(
+            id = StressLevelResource.One.id,
+            stressLevel = StressLevelResource.One,
+        )
+
+        val stressLevelRecordResourceTwoPreview = StressLevelRecordResource(
+            id = StressLevelResource.Two.id,
+            stressLevel = StressLevelResource.Two,
+        )
+
+        val stressLevelRecordResourceThreePreview = StressLevelRecordResource(
+            id = StressLevelResource.Three.id,
+            stressLevel = StressLevelResource.Three,
+        )
+
+        val stressLevelRecordResourceFourPreview = StressLevelRecordResource(
+            id = StressLevelResource.Four.id,
+            stressLevel = StressLevelResource.Four,
+        )
+
+        val stressLevelRecordResourceFivePreview = StressLevelRecordResource(
+            id = StressLevelResource.Five.id,
+            stressLevel = StressLevelResource.Five,
+        )
+
+        val stressLevelRecordResourceWithStressorsPreview = StressLevelRecordResource(
+            id = 6,
+            stressLevel = StressLevelResource.Five,
+            stressors = allStressors,
+        )
+    }
+}

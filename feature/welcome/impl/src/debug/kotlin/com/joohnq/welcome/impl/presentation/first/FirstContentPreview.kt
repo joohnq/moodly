4271ee4233294876d882802927d9cf0b29481863
João Henrique <joaohenriquess3287@gmail.com>
2025-07-24 13:13:14 -0300

    feat(:feature:welcome:impl): creating debug and putting previews

---
+++ feature/welcome/impl/src/debug/kotlin/com/joohnq/welcome/impl/presentation/first/FirstContentPreview.kt
@@ -0,0 +1,10 @@
+package com.joohnq.welcome.impl.presentation.first
+
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun FirstContentPreview() {
+    FirstContent()
+}
---
+++ feature/welcome/impl/src/debug/kotlin/com/joohnq/welcome/impl/presentation/welcome/WelcomeContentPreview.kt
@@ -0,0 +1,61 @@
+package com.joohnq.welcome.impl.presentation.welcome
+
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.runtime.Composable
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun WelcomeContentOnePreview() {
+    WelcomeContent(
+        pagerState = rememberPagerState(0) { 6 },
+    )
+}
+
+@Preview
+@Composable
+fun WelcomeContentTwoPreview() {
+    WelcomeContent(
+        pagerState = rememberPagerState(1) { 6 },
+    )
+}
+
+@Preview
+@Composable
+fun WelcomeContentThreePreview() {
+    WelcomeContent(
+        pagerState = rememberPagerState(2) { 6 },
+    )
+}
+
+@Preview
+@Composable
+fun WelcomeContentFourPreview() {
+    WelcomeContent(
+        pagerState = rememberPagerState(3) { 6 },
+    )
+}
+
+@Preview
+@Composable
+fun WelcomeContentFivePreview() {
+    WelcomeContent(
+        pagerState = rememberPagerState(4) { 6 },
+    )
+}
+
+@Preview
+@Composable
+fun WelcomeContentSixPreview() {
+    WelcomeContent(
+        pagerState = rememberPagerState(5) { 6 },
+    )
+}
+
+@Preview
+@Composable
+fun WelcomeContentSevenPreview() {
+    WelcomeContent(
+        pagerState = rememberPagerState(6) { 6 },
+    )
+}

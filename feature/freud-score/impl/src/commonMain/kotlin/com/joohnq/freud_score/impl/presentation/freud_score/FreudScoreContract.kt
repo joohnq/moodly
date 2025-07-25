5656ee93cf0381059e4a6bd9e71ba0edd32f7db7
João Henrique <joaohenriquess3287@gmail.com>
2025-07-25 13:56:09 -0300

    feat(:feature:freudScore:impl): applying BaseViewModel to ViewModels

--- feature/freud-score/impl/src/commonMain/kotlin/com/joohnq/freud_score/impl/presentation/freud_score/FreudScoreContract.kt
+++ feature/freud-score/impl/src/commonMain/kotlin/com/joohnq/freud_score/impl/presentation/freud_score/FreudScoreContract.kt
@@ -2,11 +2,16 @@ package com.joohnq.freud_score.impl.presentation.freud_score
 
 import com.joohnq.freud_score.impl.resource.FreudScoreResource
 import com.joohnq.mood.impl.ui.resource.MoodRecordResource
+import com.joohnq.ui.UnidirectionalViewModel
 
 sealed interface FreudScoreContract {
+    interface ViewModel : UnidirectionalViewModel<State, Intent, SideEffect>
+
     sealed interface Intent {
         data class Get(val records: List<MoodRecordResource>) : Intent
     }
+
+    sealed interface SideEffect
 
     data class State(
         val freudScore: FreudScoreResource? = null,
--- feature/freud-score/impl/src/commonMain/kotlin/com/joohnq/freud_score/impl/presentation/freud_score/FreudScoreViewModel.kt
+++ feature/freud-score/impl/src/commonMain/kotlin/com/joohnq/freud_score/impl/presentation/freud_score/FreudScoreViewModel.kt
@@ -1,26 +1,20 @@
 package com.joohnq.freud_score.impl.presentation.freud_score
 
-import androidx.lifecycle.ViewModel
 import com.joohnq.freud_score.impl.mapper.toResource
 import com.joohnq.mood.impl.ui.mapper.calculateStatsFreudScore
-import com.joohnq.mood.impl.ui.resource.MoodRecordResource
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
+import com.joohnq.ui.BaseViewModel
 
-class FreudScoreViewModel() :
-    ViewModel() {
-    private val _state = MutableStateFlow(FreudScoreContract.State())
-    val state: StateFlow<FreudScoreContract.State> = _state.asStateFlow()
-
-    fun onAction(intent: FreudScoreContract.Intent) {
+class FreudScoreViewModel(
+    initialState: FreudScoreContract.State = FreudScoreContract.State(),
+) : BaseViewModel<FreudScoreContract.State, FreudScoreContract.Intent, FreudScoreContract.SideEffect>(
+    initialState = initialState
+), FreudScoreContract.ViewModel {
+    override fun onIntent(intent: FreudScoreContract.Intent) {
         when (intent) {
-            is FreudScoreContract.Intent.Get -> getFreudScore(intent.records)
+            is FreudScoreContract.Intent.Get ->
+                updateState {
+                    it.copy(freudScore = intent.records.calculateStatsFreudScore().toResource())
+                }
         }
-    }
-
-    private fun getFreudScore(records: List<MoodRecordResource>) {
-        _state.update { it.copy(freudScore = records.calculateStatsFreudScore().toResource()) }
     }
 }

9e89f1847feaa3e27dd0535b5b26e6dd60af841d
João Henrique <joaohenriquess3287@gmail.com>
2025-07-25 13:09:15 -0300

    feat(:core:ui): Introduce UnidirectionalViewModel and BaseViewModel

---
+++ core/ui/src/commonMain/kotlin/com/joohnq/ui/UnidirectionalViewModel.kt
@@ -0,0 +1,10 @@
+package com.joohnq.ui
+
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+
+interface UnidirectionalViewModel<STATE, INTENT, EFFECT> {
+    val state: StateFlow<STATE>
+    val effect: SharedFlow<EFFECT>
+    fun onIntent(intent: INTENT)
+}

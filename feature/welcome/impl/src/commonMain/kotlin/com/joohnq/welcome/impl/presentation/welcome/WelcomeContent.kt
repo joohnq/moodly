abbd5fd5cb8121b6fa7fc2a48c83b5083e0fd4ad
João Henrique <joaohenriquess3287@gmail.com>
2025-07-26 08:43:20 -0300

    refactor(:feature:welcome): applying lint

--- feature/welcome/impl/src/commonMain/kotlin/com/joohnq/welcome/impl/presentation/welcome/WelcomeContent.kt
+++ feature/welcome/impl/src/commonMain/kotlin/com/joohnq/welcome/impl/presentation/welcome/WelcomeContent.kt
@@ -37,7 +37,7 @@ fun WelcomeContent(
     snackBarState: SnackbarHostState = rememberSnackBarState(),
     pagerState: PagerState,
     onNext: () -> Unit = {},
-    onAction: (PreferencesContract.Intent) -> Unit = {},
+    onAction: (PreferencesContract.Intent) -> Unit = {}
 ) {
     AppScaffoldLayout(
         snackBarHostState = snackBarState,
@@ -49,79 +49,88 @@ fun WelcomeContent(
         ) { page ->
             when (page) {
                 0 -> FirstScreen(onNext = onNext)
-                1 -> WelcomeBase(
-                    welcome = Welcome(
-                        image = Drawables.Images.WelcomeHealthState,
-                        step = Res.string.one,
-                        index = 1,
-                        backgroundColor = Colors.Green30,
-                        firstTitle = Res.string.health_state_first_title,
-                        secondTitle = Res.string.health_state_second_title,
-                        span = Res.string.health_state_span,
-                        spanColor = Colors.Green50,
-                    ),
-                    paddingTop = padding.calculateTopPadding(),
-                    onNext = onNext
-                )
+                1 ->
+                    WelcomeBase(
+                        welcome =
+                            Welcome(
+                                image = Drawables.Images.WelcomeHealthState,
+                                step = Res.string.one,
+                                index = 1,
+                                backgroundColor = Colors.Green30,
+                                firstTitle = Res.string.health_state_first_title,
+                                secondTitle = Res.string.health_state_second_title,
+                                span = Res.string.health_state_span,
+                                spanColor = Colors.Green50
+                            ),
+                        paddingTop = padding.calculateTopPadding(),
+                        onNext = onNext
+                    )
 
+                2 ->
+                    WelcomeBase(
+                        welcome =
+                            Welcome(
+                                image = Drawables.Images.WelcomeIntelligent,
+                                step = Res.string.two,
+                                index = 2,
+                                backgroundColor = Colors.Orange20,
+                                secondTitle = Res.string.intelligent_first_second_title,
+                                span = Res.string.intelligent_first_span,
+                                spanColor = Colors.Orange50
+                            ),
+                        paddingTop = padding.calculateTopPadding(),
+                        onNext = onNext
+                    )
 
-                2 -> WelcomeBase(
-                    welcome = Welcome(
-                        image = Drawables.Images.WelcomeIntelligent,
-                        step = Res.string.two,
-                        index = 2,
-                        backgroundColor = Colors.Orange20,
-                        secondTitle = Res.string.intelligent_first_second_title,
-                        span = Res.string.intelligent_first_span,
-                        spanColor = Colors.Orange50,
-                    ),
-                    paddingTop = padding.calculateTopPadding(),
-                    onNext = onNext
-                )
+                3 ->
+                    WelcomeBase(
+                        welcome =
+                            Welcome(
+                                image = Drawables.Images.WelcomeMental,
+                                step = Res.string.three,
+                                index = 3,
+                                backgroundColor = Colors.Gray10,
+                                firstTitle = Res.string.ai,
+                                secondTitle = Res.string.journaling_ai_therapy_chatbot,
+                                span = Res.string.mental,
+                                spanColor = Colors.Gray60
+                            ),
+                        paddingTop = padding.calculateTopPadding(),
+                        onNext = onNext
+                    )
 
-                3 -> WelcomeBase(
-                    welcome = Welcome(
-                        image = Drawables.Images.WelcomeMental,
-                        step = Res.string.three,
-                        index = 3,
-                        backgroundColor = Colors.Gray10,
-                        firstTitle = Res.string.ai,
-                        secondTitle = Res.string.journaling_ai_therapy_chatbot,
-                        span = Res.string.mental,
-                        spanColor = Colors.Gray60,
-                    ),
-                    paddingTop = padding.calculateTopPadding(),
-                    onNext = onNext
-                )
+                4 ->
+                    WelcomeBase(
+                        welcome =
+                            Welcome(
+                                image = Drawables.Images.WelcomeResources,
+                                step = Res.string.four,
+                                index = 4,
+                                backgroundColor = Colors.Yellow20,
+                                firstTitle = Res.string.resource_first_title,
+                                span = Res.string.resource_span,
+                                secondTitle = Res.string.resource_second_title,
+                                spanColor = Colors.Yellow60
+                            ),
+                        paddingTop = padding.calculateTopPadding(),
+                        onNext = onNext
+                    )
 
-                4 -> WelcomeBase(
-                    welcome = Welcome(
-                        image = Drawables.Images.WelcomeResources,
-                        step = Res.string.four,
-                        index = 4,
-                        backgroundColor = Colors.Yellow20,
-                        firstTitle = Res.string.resource_first_title,
-                        span = Res.string.resource_span,
-                        secondTitle = Res.string.resource_second_title,
-                        spanColor = Colors.Yellow60,
-                    ),
-                    paddingTop = padding.calculateTopPadding(),
-                    onNext = onNext
-                )
-
-                5 -> WelcomeBase(
-                    welcome = Welcome(
-                        image = Drawables.Images.WelcomeCommunity,
-                        step = Res.string.five,
-                        index = 5,
-                        backgroundColor = Colors.Purple30,
-                        firstTitle = Res.string.community_first_title,
-                        span = Res.string.community_span,
-                        spanColor = Colors.Purple40,
-                    ),
-                    paddingTop = padding.calculateTopPadding(),
-                    onNext = { onAction(PreferencesContract.Intent.UpdateSkipWelcome()) }
-                )
+                5 ->
+                    WelcomeBase(
+                        welcome =
+                            Welcome(
+                                image = Drawables.Images.WelcomeCommunity,
+                                step = Res.string.five,
+                                index = 5,
+                                backgroundColor = Colors.Purple30,
+                                firstTitle = Res.string.community_first_title,
+                                span = Res.string.community_span,
+                                spanColor = Colors.Purple40
+                            ),
+                        paddingTop = padding.calculateTopPadding(),
+                        onNext = { onAction(PreferencesContract.Intent.UpdateSkipWelcome()) }
+                    )
             }
         }
     }

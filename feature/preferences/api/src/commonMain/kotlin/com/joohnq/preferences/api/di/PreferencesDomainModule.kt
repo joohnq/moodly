8952ebfca441189f4752a57de3cfd994b9cacb58
João Henrique <joaohenriquess3287@gmail.com>
2025-07-28 13:34:18 -0300

    refactor(:feature:preferences): lint

--- feature/preferences/api/src/commonMain/kotlin/com/joohnq/preferences/api/di/PreferencesDomainModule.kt
+++ feature/preferences/api/src/commonMain/kotlin/com/joohnq/preferences/api/di/PreferencesDomainModule.kt
@@ -1,6 +1,10 @@
 package com.joohnq.preferences.api.di
 
-import com.joohnq.preferences.api.use_case.*
+import com.joohnq.preferences.api.use_case.GetUserPreferencesUseCase
+import com.joohnq.preferences.api.use_case.UpdateSkipAuthUseCase
+import com.joohnq.preferences.api.use_case.UpdateSkipOnboardingUseCase
+import com.joohnq.preferences.api.use_case.UpdateSkipSecurityUseCase
+import com.joohnq.preferences.api.use_case.UpdateSkipWelcomeUseCase
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 

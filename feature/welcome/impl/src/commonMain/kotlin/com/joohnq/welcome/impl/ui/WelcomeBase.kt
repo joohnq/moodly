5938be1a379cf3f850711ea0d20f7591c9438a95
João Henrique <joaohenriquess3287@gmail.com>
2025-07-28 13:35:05 -0300

    refactor(:feature:welcome): lint

--- feature/welcome/impl/src/commonMain/kotlin/com/joohnq/welcome/impl/ui/WelcomeBase.kt
+++ feature/welcome/impl/src/commonMain/kotlin/com/joohnq/welcome/impl/ui/WelcomeBase.kt
@@ -1,7 +1,14 @@
 package com.joohnq.welcome.impl.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment

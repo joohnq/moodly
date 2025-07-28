a62dd73df914a84faa4ee6ceb0360e4dedbfdc7c
João Henrique <joaohenriquess3287@gmail.com>
2025-07-28 09:17:13 -0300

    fix: merge main

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import com.diffplug.gradle.spotless.SpotlessPlugin
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
@@ -8,6 +11,39 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.crashlytics) apply false
     alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.spotless) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.android.lint) apply false
+}
+
+subprojects{
+    apply<SpotlessPlugin>()
+    configure<SpotlessExtension> {
+        kotlin {
+            target("**.kt")
+            targetExclude("build/**/*.kt")
+            ktlint(libs.versions.ktlint.get())
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+
+        kotlinGradle {
+            target("**/*.gradle.kts")
+            ktlint(libs.versions.ktlint.get())
+                .editorConfigOverride(
+                    mapOf(
+                        "indent_size" to "4",
+                        "continuation_indent_size" to "4"
+                    )
+                )
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+
+        format("misc") {
+            target("**/*.md", "**/.gitignore", "**/*.properties")
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+    }
 }
